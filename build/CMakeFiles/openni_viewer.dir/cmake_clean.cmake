47c2e213bf3af239976462153ab3232170d45261
mm89039 <marikamurphy18@gmail.com>
2019-10-02 12:16:18 -0500

    first commit

---
+++ build/CMakeFiles/openni_viewer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openni_viewer.dir/src/openni_viewer_simple.cpp.o"
+  "openni_viewer.pdb"
+  "openni_viewer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openni_viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
