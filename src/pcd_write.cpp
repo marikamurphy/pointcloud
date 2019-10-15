dea470404622a43a8ff4a78a00bcc61f0274a842
jsuriadinata <jsuriadinata@utexas.edu>
2019-10-15 17:52:48 -0500

    successfully capture 72 point clouds, have not tested arduino code

--- src/pcd_write.cpp
+++ src/pcd_write.cpp
@@ -21,11 +21,25 @@ This code is created purely for educational reasons
 #include <pcl/io/openni_grabber.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/io/pcd_io.h>
+#include <string>
+#include <iostream> //write to console
+#include <fstream> //send data to arduino
+#include <boost/thread/thread.hpp> //sleep 
+
+#define NUM_ROTATIONS 72
 
 typedef pcl::PointXYZRGBA PointType;
+void rotate();
+void savePointCloudToFile(pcl::PointCloud<PointType>::ConstPtr cloud, int cloudNumber);
+
 
 int main( int argc, char* argv[] )
 {
+    //track how many point clouds have been saved
+    static int cloudNumber = 0;
+    //see if user has pressed space key to begin capture
+    static bool startedCapture = false;
+
     // PCL Visualizer
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
         new pcl::visualization::PCLVisualizer( "Point Cloud Viewer" ) );
@@ -58,8 +72,10 @@ int main( int argc, char* argv[] )
             if( (int)event.getKeyCode() == 32 && event.keyDown() ){
                 boost::mutex::scoped_try_lock lock( mutex );
                 if( lock.owns_lock() ){
-                    pcl::io::savePCDFile( "pcd_files/cloud.pcd", *cloud, true ); // Binary format
-                    //pcl::io::savePCDFile( "cloud.pcd", *cloud, false ); // ASCII format
+                    if(cloudNumber < NUM_ROTATIONS && startedCapture == false){
+                        startedCapture = true;
+                    }
+                        
                 }
             }
         };
@@ -69,6 +85,14 @@ int main( int argc, char* argv[] )
 
     // Start Grabber
     grabber->start();
+
+    //connect to the arduino
+    //open arduino device file (linux)
+    std::ofstream arduino;
+	arduino.open( "/dev/ttyACM0"); //TODO
+    //TODO: check that connection was established
+
+    std::cout << "Press space key to begin capturing" << endl;
 
     while( !viewer->wasStopped() ){
         // Update Viewer
@@ -84,7 +108,44 @@ int main( int argc, char* argv[] )
                     viewer->addPointCloud( cloud, "cloud" );
                     viewer->resetCameraViewpoint( "cloud" );
                 }
+                //need to rotate NUM_ROTATIONS and save each
+                if(cloudNumber < NUM_ROTATIONS && startedCapture){
+                    //save point cloud
+                    savePointCloudToFile(cloud, cloudNumber);
+                    //rotate the turntable
+                    rotate();
+                    cloudNumber++;
+                }
+                else if(cloudNumber >= NUM_ROTATIONS){
+                    arduino.close();
+                    std::cout << "Finished capture" << endl;
+                    return 0;
+                }
+               
             }
         }
     }
+}
+
+//send signal to arduino to rotate and sleep to make sure that it completes
+void rotate(){
+    
+        cout << "rotating signal sent" << endl;
+        //send signal to arduino to rotate the stepper motor
+        arduino << "rotate"; //TODO: determine if appropriate to send string or say int
+        //sleep to wait for rotation to happen
+        boost::this_thread::sleep( boost::posix_time::milliseconds(500) );
+        cout << "finished rotating" << endl;
+}
+
+//save each captured point cloud to .pcd file in the pcd_files folder (in build folder)
+void savePointCloudToFile(pcl::PointCloud<PointType>::ConstPtr cloud, int cloudNumber){
+    std::string file_name = "cloud.pcd"; 
+    std::string file_path = "pcd_files/";
+    std::stringstream full_file_path;
+    full_file_path << file_path + std::to_string(cloudNumber) + file_name;
+    pcl::io::savePCDFile( full_file_path.str(), *cloud, true ); // Binary format
+    //pcl::io::savePCDFile( full_file_path.str(), *cloud, false ); // ASCII format
+    std::cout << "Saving cloud #" << cloudNumber << endl;
+    
 }
